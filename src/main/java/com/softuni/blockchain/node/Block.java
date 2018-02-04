dab99d602dd6b72afc3301c36f912be931da281e
holymir <vtsochev@abv.bg>
2018-02-04 12:04:43 +0200

    added block handler

--- src/main/java/com/softuni/blockchain/node/Block.java
+++ src/main/java/com/softuni/blockchain/node/Block.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Block {
 
-    private int index;              //number
-    private List<Transaction> transactions;   //Transaction[]
+    private int index;
+    private List<Transaction> transactions;
     private int difficulty;         //number
     private String prevBlockHash;   //hex_number
     private String minedBy;         //address
@@ -16,4 +16,75 @@ public class Block {
     private Long dateCreated;       //timestamp
     private String blockHash;       //hex_number
 
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getPrevBlockHash() {
+        return prevBlockHash;
+    }
+
+    public void setPrevBlockHash(String prevBlockHash) {
+        this.prevBlockHash = prevBlockHash;
+    }
+
+    public String getMinedBy() {
+        return minedBy;
+    }
+
+    public void setMinedBy(String minedBy) {
+        this.minedBy = minedBy;
+    }
+
+    public String getBlockDataHash() {
+        return blockDataHash;
+    }
+
+    public void setBlockDataHash(String blockDataHash) {
+        this.blockDataHash = blockDataHash;
+    }
+
+    public Long getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(Long nonce) {
+        this.nonce = nonce;
+    }
+
+    public Long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getBlockHash() {
+        return blockHash;
+    }
+
+    public void setBlockHash(String blockHash) {
+        this.blockHash = blockHash;
+    }
 }
--- src/main/java/com/softuni/blockchain/node/NodeController.java
+++ src/main/java/com/softuni/blockchain/node/NodeController.java
@@ -1,6 +1,7 @@
 package com.softuni.blockchain.node;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,13 +12,20 @@ public class NodeController {
 
         return new Node();
 
-//        return "\"about\": \"SoftUniChain/0.9-java\",\n" +
-//                "  \"nodeName\": \"Sofia-01\",\n" +
-//                "  \"peers\": 2,\n" +
-//                "  \"blocks\": 25,\n" +
-//                "  \"confirmedTransactions\": 208,\n" +
-//                "  \"pendingTransactions\": 7,\n" +
-//                "  \"addresses\": 12,\n" +
-//                "  \"coins\": 18000000\n";
     }
+
+    @GetMapping("/blocks")
+    public Block block() {
+
+        return new Block();
+    }
+
+    @GetMapping("/blocks/{index}")
+    public Block block(@PathVariable Integer index) {
+        Block block =  new Block();
+        block.setIndex(index);
+
+        return block;
+    }
+
 }
