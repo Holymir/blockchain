7d32d2f3ade3388674973a58e941616af477d4a0
Nikolay Panayotov <nickpanaiotov@gmail.com>
2018-02-04 17:39:21 +0200

    Changed add peer logic.

--- src/main/java/com/softuni/blockchain/node/PeerController.java
+++ src/main/java/com/softuni/blockchain/node/PeerController.java
@@ -12,18 +12,35 @@ import java.util.Set;
 @Component
 public class PeerController {
     static Set<Peer> peers = new HashSet<>();
+    private Peer me;
+
+    public PeerController() {
+        this.me = new Peer("192.168.0.11:8008");
+    }
 
     public List<Peer> addPeer(Peer peer) {
         try {
             RestTemplate restTemplate = new RestTemplate();
-            Node node = restTemplate.getForObject("http://" + peer.getUrl() + "/info", Node.class);
+            restTemplate.getForObject("http://" + peer.getUrl() + "/info", Node.class);
+
+            this.addMeAsPeer(peer);
+
+            peers.add(peer);
+
+            return new ArrayList<>(peers);
         } catch (final HttpClientErrorException e) {
             System.out.println("Node not available");
             System.out.println(e.getMessage());
         }
 
-        peers.add(peer);
-        return new ArrayList<>(peers);
+        return null;
+    }
+
+    private void addMeAsPeer(Peer peer) {
+        if (!peer.equals(me) || peers.contains(peer)) {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.postForObject("http://" + peer.getUrl() + "/peer", this.me, Peer[].class);
+        }
     }
 }
 
