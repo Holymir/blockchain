c37d0b8fcc1994d705d5b91cfd72ca84f104e41d
Nikolay Panayotov <nickpanaiotov@gmail.com>
2018-02-06 01:37:22 +0200

    Removed global serializer, we will use only specific one for the core logic.

--- src/main/java/com/softuni/blockchain/utils/Utils.java
+++ src/main/java/com/softuni/blockchain/utils/Utils.java
@@ -3,20 +3,27 @@ package com.softuni.blockchain.utils;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 
 public class Utils {
 
+    private static ObjectMapper objectMapper;
+
+    static {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
+        builder.featuresToEnable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+        builder.failOnUnknownProperties(false);
+
+        objectMapper = builder.build();
+    }
+
     public static String serialize(Object object) {
         try {
-            Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-            builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-            builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
-            builder.featuresToEnable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
-            builder.failOnUnknownProperties(false);
-
-            return builder.build().writeValueAsString(object);
+            return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
